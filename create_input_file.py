1fda43a249474f78e64f5bb888dbf5645bb431af
Matas Lauzadis <matas.lauzadis@gmail.com>
2019-05-31 13:36:41 -0500

    Update create_input_file.py

--- create_input_file.py
+++ create_input_file.py
@@ -5,7 +5,6 @@ with open('./data/input_data/group_characteristics/Selected_BG_inputs_20180208.c
     reader = csv.reader(characteristics_file)
     next(reader)
     for line in reader:
-        # print(line[0])
         print(fips_converter.fips_conversion(line[0]))
 
 
