fdbfb1041511188b48fde56f8ed17e5085221923
mataslauzadis <30608308+mataslauzadis@users.noreply.github.com>
2019-08-31 23:36:33 -0500

    Made functions to create input files for all three scenarios

--- plot_maker.py
+++ plot_maker.py
@@ -1,37 +1,36 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from fips_converter import fips_conversion
+import glob
 from swmm import complete_process
 
-# Alabama, Autauga County
-frame = pd.read_csv('./data/binary_csv/010010202001.csv', delimiter=',')
-print(frame)
-frame.columns = ['date', 'rainfall', 'evaporation', 'runoff', 'total_lat_inflow', 'flow_leaving_outfalls', 'evaporation_rate', 'potential_PET']
-frame['balance'] = frame['rainfall'] - frame['evaporation']
-# frame['date'] = frame['date'].to_datetime()
-# frame = frame['date'].to_frame().join(frame['balance'])
 
-# print(frame.resample('d').sum())
+def make_plot(csv_file):
+    geoid = csv_file[7:19]
+    geoid_data = fips_conversion(geoid)
 
+    frame = pd.read_csv(csv_file, delimiter=',')
+    print(frame)
+    frame.columns = ['date', 'rainfall', 'evaporation', 'runoff', 'total_lat_inflow', 'flow_leaving_outfalls', 'evaporation_rate', 'potential_PET']
+    frame['balance'] = frame['rainfall'] - frame['evaporation']
 
-date = []
-balance = []
-for x, y in frame.iterrows():
-    date.append(x+1)
-    balance.append(y['balance'])
+    date = []
+    balance = []
+    for x, y in frame.iterrows():
+        date.append(x+1)
+        balance.append(y['balance'])
 
-monthly_balance = 0
-for i in range(len(balance[:30])):
-    monthly_balance += balance[i]
+    # monthly_balance = 0
+    # for i in range(len(balance[:30])):
+    #     monthly_balance += balance[i]
+    # print(monthly_balance)
 
-print(monthly_balance)
+    plt.plot(date[:365], [a if a > 0 else 0 for a in balance[:365]])
+    # plt.plot(date[:1825], balance[:1825])  # Negative plot
 
-
-
-plt.plot(date[:365], [a if a > 0 else 0 for a in balance[:365]])
-plt.xlabel('Days since January 1st, 1981')
-plt.ylabel('Water Level (Inches)')
-plt.title('Water Level over Time in Autauga County, Alabama')
-plt.tight_layout()
-plt.savefig('./data/plot_spring.svg')
+    plt.ylabel('Water Level (Inches)')
+    plt.title('Water Level over Time in ' + geoid_data[1] + ' County, ' + geoid_data[0] + ' - ' + geoid)
+    plt.tight_layout()
+    plt.savefig('./data/' + geoid + '.svg')
 
 
