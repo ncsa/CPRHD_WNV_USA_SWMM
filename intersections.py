fe91b818d42c2c17876be2b2b7128c2639e77b17
Matas <matas.lauzadis@gmail.com>
2019-09-17 14:11:14 -0500

    Added projection parameter to intersections function, improved documentation

--- intersections.py
+++ intersections.py
@@ -7,11 +7,17 @@ from multiprocessing import Pool
 from functools import partial
 
 
-def intersections(raster_file, shape_file):
-    # Preconditions: A shape file and raster file have been passed, each having the same projection.
-    # Postconditions: A pandas dataframe has been returned, holding the min, max, mean, and count for each polygon in the shape file.
+def intersections(raster_file, shape_file, projection='4326'):
+    # INPUTS:
+    #   raster_file: path to the raster file
+    #   shape_file: path to the shape file
+    #   projection (optional): EPSG code of the projection of your shape file (default is 4326)
+
+    # OUTPUT:
+    #   frame: pandas dataframe that contains the min, max, and mean values for each feature in the shape file
+
     shape_frame = gpd.read_file(shape_file)
-    shape = shape_frame.to_crs({'init': 'epsg:4326', 'no_defs': True})  # Change the projection to your shapefile's projection
+    shape = shape_frame.to_crs({'init': 'epsg:' + projection, 'no_defs': True})  # Change the projection to your input projection
 
     output_columns = ['min', 'max', 'mean']
     stats = rs.zonal_stats(shape, raster_file, stats=output_columns, all_touched=True)
@@ -22,22 +28,23 @@ def intersections(raster_file, shape_file):
     return frame
 
 
-def multithread_helper(raster_file, shape_file):
+def multithread_helper(raster_file, shape_file, projection='4326'):
+    # Multithread helper function so each thread can save the frame to the disk as a csv
     filename = raster_file[-15:-8]
     print(filename)
-    frame = intersections(raster_file, shape_file)
+    frame = intersections(raster_file, shape_file, projection)
     frame.to_csv('../narr_data_fix/rhum.2m/' + filename + '.csv')
 
 
 # SINGLE THREADED OPERATION
-# shape_file = '../narr_data_fix/urban_counties/urban_counties_wnv_4326.shp'
+shape_file = '../narr_data_fix/urban_counties/urban_counties_wnv_4326.shp'
 # file_list = glob.glob('../narr_data_fix/air.2m_geotiff/*.geotiff')
 # for file in tqdm(file_list):
 #     frame = intersections(file, shape_file)
 
 
-# MULTI THREADED OPERATION
+# # MULTI THREADED OPERATION
 # if __name__ == '__main__':
 #     file_list = glob.glob('../narr_data_fix/rhum.2m_geotiff/*.geotiff')
 #     pool = Pool()
-#     pool.map(partial(multithread_helper, shape_file=shape_file), file_list)
+#     pool.map(partial(multithread_helper, shape_file=shape_file, projection='4326'), file_list)
