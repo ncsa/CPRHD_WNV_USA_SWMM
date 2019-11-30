526b78331251bc92f381da5de64a6a44edf61f24
Matas Lauzadis <matas.lauzadis@gmail.com>
2019-11-30 12:46:36 -0600

    Removed old files

--- jupyter_notebooks/SWMM_Precipitation/swmm_analysis/simulation_files/chicago/test.py
+++
@@ -1,9 +0,0 @@
-from pyswmm import Simulation
-import glob
-
-if __name__ == '__main__':
-	files = glob.glob('./*.inp')
-	for file in files:
-		print(file)
-		sim = Simulation(file)
-		sim.execute()
