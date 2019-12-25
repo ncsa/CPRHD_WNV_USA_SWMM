b5157d2d1623eb1659ac2b29b5fb8786c6d22932
Matas Lauzadis <matas.lauzadis@gmail.com>
2019-12-24 23:49:56 -0600

    Implemented disk-based queue for running model

--- python/multiprocess_simulation.py
+++ python/multiprocess_simulation.py
@@ -1,8 +1,7 @@
 import glob
+import persistqueue
 from pyswmm import Simulation
-import pickle
-from multiprocessing import Pool
-import os
+from multiprocessing import Process
 
 
 _sim_types = {'ng': 'no_green_infrastructure',
@@ -11,61 +10,35 @@ _sim_types = {'ng': 'no_green_infrastructure',
               }
 
 
-_batch_size = 32
+_sim_type = 'ng'
+_max_processes = 8
 
 
-def run_simulation(file):
-    sim = Simulation(file)
-    sim.execute()
-
-
-def chonk(list, chonk_size):
-    for i in range(0, len(list), chonk_size):
-        yield list[i: i + chonk_size]
+path_to_input_files = './' + _sim_types[_sim_type] + '/'
+path_to_queue = path_to_input_files + _sim_types[_sim_type] + '_queue'
+q = persistqueue.UniqueAckQ(path_to_queue, auto_commit=True, multithreading=True)
 
 
 def main():
-    # Specify Simulation Type
-    sim_type = 'ng'
+    if q.size == 0:
+        input_files = glob.glob(path_to_input_files + '*.inp')
+        print('Queue is empty, adding', len(input_files), 'files from', path_to_input_files)
+        for file in input_files:
+            q.put(file)
 
-    chunk_file = '../data/input_files/' + sim_type + '_chunked'
-    completed_chunk_file = '../data/input_files/' + sim_type + '_chunked_completed'
-    path_to_input_files = '../input_files/' + _sim_types[sim_type] + '/*inp'
+    print('Queue Size:', q.size)
+    for i in range(_max_processes):
+        p = Process(target=worker)
+        p.start()
+    return
 
-    if not os.path.exists(chunk_file):
-	# Create and save the initial chunk list
-        print('Chunk file not found, making a new one at:', chunk_file)
-        files = glob.glob(path_to_input_files)
-        chunks = list(chonk(files, _batch_size))
-        pickle.dump(chunks, open(chunk_file, 'wb'))
-    
-    else: # Load the existing chunk list
-        chunks = pickle.load(open(chunk_file, 'rb'))
 
-    print('Number of batches:', len(chunks))
-    print('Batch Size:', _batch_size)
-
-    pool = Pool()
-    while len(chunks) > 0:
-        chunk = chunks[0]
-        # Multiprocess the chunk
-        pool.map(run_simulation, chunk)
-
-        # Save the completed chunks
-        try:
-            completed_chunks = pickle.load(open(completed_chunk_file, 'rb'))
-        except:  # if the file doesn't exist yet (first completed chunk)
-            completed_chunks = []
-        completed_chunks.append(chunk)
-        pickle.dump(completed_chunks, open(completed_chunk_file, 'wb'))
-
-        # Remove the completed chunk from the list
-        chunks.remove(chunks[0])
-
-        # Save
-        pickle.dump(chunks, open(chunk_file, 'wb'))
-
-    print('No more batches left to process')
+def worker():
+    while True:
+        file = q.get(block=True)
+        sim = Simulation(file)
+        sim.execute()
+        q.ack(file)
 
 
 if __name__ == '__main__':
